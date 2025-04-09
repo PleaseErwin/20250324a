b3de666d2363e8d406ed91bc5e9e6d23f90496df
PleaseErwin <rosec77@sookmyung.ac.kr>
2025-04-09 09:45:16 +0900

    [UPDATE] test src

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -2,12 +2,17 @@ package edu.fisa.ce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Step07CicdApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    public void testAddition() {
+        int result = 2 + 3;
+        System.out.println("비교 전");
+        assertEquals(5, result);  // 성공: 5 == 5
+        System.out.println("비교 후");
+    }
 
 }
